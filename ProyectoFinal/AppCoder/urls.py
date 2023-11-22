08ab68c801cbd7588fb3344b660707012f5e289c
Nazareno <nazareno@.com>
2023-11-22 18:58:12 -0300

    error

--- ProyectoFinal/AppCoder/urls.py
+++ ProyectoFinal/AppCoder/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from django.http import HttpResponse
 
-def view_inicio(request):
-  return HttpResponse("Bienvenidos")
+from AppCoder.views import inicio_view, cursos_view
 
 urlpatterns = [
-  path("inicio/", view_inicio),
+  path("inicio/", inicio_view),
+  path("cursos/", cursos_view),
 ]
