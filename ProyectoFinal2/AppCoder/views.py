7f9b972dce655931e5d1484d85aecdb4574eeb97
Nazareno <nazareno@.com>
2023-11-23 17:55:14 -0300

    clase jueves

--- ProyectoFinal2/AppCoder/views.py
+++ ProyectoFinal2/AppCoder/views.py
@@ -4,6 +4,28 @@ from django.http import HttpResponse
 def inicio_view(request):
   return HttpResponse("Bienvenidos")
 
+from django.template import Template, Context
+from django.http import HttpResponse
+
 def cursos_view(request):
   #return HttpResponse("Estos son mis cursos")
   return render(request, "AppCoder/padre.html")
+
+def cursos_view(xx):
+    nombre = "Mariano Manuel"
+    apellido = "Barracovich"
+    diccionario = {
+        'nombre': nombre,
+        'apellido': apellido,
+        "nacionalidad": "argentino"
+    }  # Para enviar al contexto
+
+    ruta = "C:/Users/nazareno/Desktop/Code/Proyecto_final/ProyectoFinal2/AppCoder/templates/AppCoder/padre.html"
+    mi_archivo = open(ruta, "r")
+
+    # "Método django - versión 1"
+    plantilla = Template(mi_archivo.read())  # Se carga en memoria nuestro documento, template1
+    contexto = Context(diccionario)  # Le doy al contexto mi nombre y apellido
+    documento = plantilla.render(contexto)  # Aqui renderizamos la plantilla en documento
+
+    return HttpResponse(documento)
